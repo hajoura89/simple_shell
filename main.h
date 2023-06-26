14ab208af55b342b8a103fb0f95f16a0468e114e
hadj abbou najlae <hadjabbou.najlae@gmail.com>
2023-06-25 19:43:06 -0700

    strings functions

---
+++ main.h
@@ -0,0 +1,185 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <limits.h>
+#include <fcntl.h>
+#include <string.h>
+#include <errno.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <sys/stat.h>
+
+#define LOWERCASE	1
+#define UNSIGNED	2
+
+extern char **environ;
+
+#define NORMAL	0
+#define OR		1
+#define AND		2
+#define CHAIN	3
+
+#define BUFFER_READ 1024
+#define BUFFER_WRITE 1024
+#define BUF_FLUSH -1
+
+#define HISTORY_FILE	".history_file"
+#define HISTORY_MAX	4096
+
+
+#define GETLINE 0
+#define STRTOK 0
+
+/**
+ * struct llstr - singly linked list
+ * @number: the number field
+ * @str: a string
+ * @nextnode: points to the next node
+ */
+typedef struct llstr
+{
+	int number;
+	char *str;
+	struct llstr *nextnode;
+} list_t;
+
+/**
+ *struct command_info - information related to command execution
+ *@arg: arguments
+ *@argv: array of strings
+ *@path: string path for the current command
+ *@argc: argument count
+ *@error_count: error count
+ *@error_code: error code for exit
+ *@linecount: on count
+ *@filename: program filename
+ *@enviro: ll local copy of environ
+ *@environ: copy of environ from LL enviro
+ *@history: history
+ *@alias: alias
+ *@environ_changed: on if environ changed
+ *@status: return status of the last executed command
+ *@cmd_buffer: buffer commande
+ *@cmd_buffer_type: command Type ||, &&, ;
+ *@read_fd: file decriptor from which to read line input
+ *@historycount: history line number count
+ */
+typedef struct command_info
+{
+	char *arg;
+	char **argv;
+	char *path;
+	int argc;
+	unsigned int error_count;
+	int error_code;
+	int linecount;
+	char *filename;
+	list_t *enviro;
+	list_t *history;
+	list_t *alias;
+	char **environ;
+	int environ_changed;
+	int status;
+	char **cmd_buffer;
+	int cmd_buffer_type;
+	int read_fd;
+	int historycount;
+} cmdinfo_t;
+
+/**
+ * struct builtin_command - Represents a built-in command
+ * @command: The built-in cmd string.
+ * @function: Pointer to the function of built-in command.
+ */
+typedef struct builtin_command
+{
+	char *command;
+	int (*function)(cmdinfo_t *);
+} builtin_command_t;
+
+int myshell(cmdinfo_t *, char **);
+int get_built_in(cmdinfo_t *);
+void get_command(cmdinfo_t *);
+void fork_command(cmdinfo_t *);
+int is_command(cmdinfo_t *, char *);
+char *duplicate_chars(char *, int, int);
+char *get_path(cmdinfo_t *, char *, char *);
+void print_input_string(char *);
+int write_char(char);
+int write_to_fd(char c, int fd);
+int print_to_fd(char *str, int fd);
+int _strlen(char *);
+int _strcmp(char *, char *);
+char *start_with(const char *, const char *);
+char *_strcat(char *, char *);
+char *_strncpy(char *, char *, int);
+char *_strcpy(char *, char *);
+char *_strduplicate(const char *);
+void _puts(char *);
+int _putchar(char);
+char *_strncat(char *, char *, int);
+char *_strchr(char *, char);
+char **_split_str(char *, char *);
+char **_split_str2(char *, char);
+void free_str(char **);
+char *_memset(char *, char, unsigned int);
+void *_realloc(void *, unsigned int, unsigned int);
+int free_ptr(void **);
+int is_interactive(cmdinfo_t *);
+int is_delimeter(char, char *);
+int _isalpha(int);
+int _atoi(char *);
+int parse_integer(char *);
+void print_error(cmdinfo_t *, char *);
+int print_decimal(int, int);
+char *convert_number(long int, int, int);
+void remove_comments(char *);
+int _shellexit(cmdinfo_t *);
+int _cd(cmdinfo_t *);
+int _printHelpMessage(cmdinfo_t *);
+int _historylist(cmdinfo_t *);
+int unset_alias(cmdinfo_t *, char *);
+int set_alias(cmdinfo_t *, char *);
+int print_alias(list_t *);
+int _alias(cmdinfo_t *);
+ssize_t input_buf(cmdinfo_t *, char **, size_t *);
+ssize_t get_input(cmdinfo_t *);
+ssize_t read_buf(cmdinfo_t *, char *, size_t *);
+int _getline(cmdinfo_t *, char **, size_t *);
+void handleSIGINT(int);
+void initialize_info(cmdinfo_t *);
+void set_info(cmdinfo_t *, char **);
+void free_info(cmdinfo_t *, int);
+int _currentenv(cmdinfo_t *);
+char *_getenvvalue(cmdinfo_t *, const char *);
+int _ssetenv(cmdinfo_t *);
+int _uunsetenv(cmdinfo_t *);
+int populate_envlist(cmdinfo_t *);
+char **get_environ(cmdinfo_t *);
+int _unsetenv(cmdinfo_t *, char *);
+int _setenv(cmdinfo_t *, char *, char *);
+char *get_hist_file(cmdinfo_t *info);
+int create_history(cmdinfo_t *info);
+int read_historyfile(cmdinfo_t *info);
+int addto_history_list(cmdinfo_t *info, char *buf, int linecount);
+int reindex_history(cmdinfo_t *info);
+list_t *add_node(list_t **, const char *, int);
+list_t *add_node_toend(list_t **, const char *, int);
+size_t print_str_list(const list_t *);
+int delete_node_at(list_t **, unsigned int);
+void free_list(list_t **);
+size_t list_length(const list_t *);
+char **list_to_string(list_t *);
+size_t print_list(const list_t *);
+list_t *node_start_with(list_t *, char *, char);
+ssize_t get_node_index(list_t *, list_t *);
+int is_chain_delimeter(cmdinfo_t *, char *, size_t *);
+void chain_check(cmdinfo_t *, char *, size_t *, size_t, size_t);
+int replace_alias(cmdinfo_t *);
+int replace_vars(cmdinfo_t *);
+int replace_string(char **, char *);
+
+#endif
---
+++ string2.c
@@ -0,0 +1,110 @@
+#include "main.h"
+
+/**
+ * _strcpy - copies a string
+ * @dest: destination
+ * @src: source
+ * Return: pointer to destination
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int i = 0;
+
+	if (dest == src || src == 0)
+		return (dest);
+	while (src[i])
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = 0;
+	return (dest);
+}
+
+/**
+ * _strduplicate - duplicates a string
+ * @str: string to duplicate
+ * Return: pointer to the result string
+ */
+char *_strduplicate(const char *str)
+{
+	int length = 0;
+	char *ret;
+
+	if (str == NULL)
+		return (NULL);
+	while (*str++)
+		length++;
+	ret = malloc(sizeof(char) * (length + 1));
+	if (!ret)
+		return (NULL);
+	for (length++; length--;)
+		ret[length] = *--str;
+	return (ret);
+}
+
+/**
+ *_puts - prints an input string
+ *@str: the string to be printed
+ * Return: Nothing
+ */
+void _puts(char *str)
+{
+	int i = 0;
+
+	if (!str)
+		return;
+	while (str[i] != '\0')
+	{
+		_putchar(str[i]);
+		i++;
+	}
+}
+
+/**
+ * _putchar - writes the character to stdout
+ * @c: The character to print
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _putchar(char c)
+{
+	static int i;
+	static char buf[BUFFER_WRITE];
+
+	if (c == BUF_FLUSH || i >= BUFFER_WRITE)
+	{
+		write(1, buf, i);
+		i = 0;
+	}
+	if (c != BUF_FLUSH)
+		buf[i++] = c;
+	return (1);
+}
+
+/**
+ **_strncat - concatenates two strings
+ *@dest: 1st string
+ *@src: 2nd string
+ *@n: nbr of bytes to be used
+ *Return: the result string
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int i, j;
+	char *s = dest;
+
+	i = 0;
+	j = 0;
+	while (dest[i] != '\0')
+		i++;
+	while (src[j] != '\0' && j < n)
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
+	if (j < n)
+		dest[i] = '\0';
+	return (s);
+}
